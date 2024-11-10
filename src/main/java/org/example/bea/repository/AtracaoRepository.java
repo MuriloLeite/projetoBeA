f13fb2aeb5e437e5bbfb6240b2d2ae4662ad31f1
MuriloLeite <murilotfleite@gmail.com>
2024-11-10 00:38:53 -0300

    alterações em feedback

--- src/main/java/org/example/bea/repository/AtracaoRepository.java
+++ src/main/java/org/example/bea/repository/AtracaoRepository.java
@@ -3,5 +3,8 @@ package org.example.bea.repository;
 import org.example.bea.model.Atracao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AtracaoRepository extends JpaRepository<Atracao, Integer> {
+    List<Atracao> findByEvento_EventoId(Integer eventoId);
 }
